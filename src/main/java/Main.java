73aee8e1dfafe1de9b22f8bf30ea61dff02f24ab
Paxton Edgar <paxtonedgar3@gmail.com>
2022-07-26 17:51:45 -0400

    init push

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,36 @@
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        // your code here
+    public static void main(String[] args) throws InterruptedException {
+        SavingsAccount bankAccount = new SavingsAccount();
+
+        Thread t1 = new Thread(() -> {
+            bankAccount.deposit(100);
+            bankAccount.withdraw(75);
+            bankAccount.withdraw(200);
+            bankAccount.deposit(25);
+            bankAccount.deposit(100);
+
+        });
+        Thread t2 = new Thread(() -> bankAccount.withdraw(50));
+
+        Thread t3 = new Thread(() -> {
+            bankAccount.withdraw(25);
+            bankAccount.deposit(100);
+            bankAccount.deposit(35);
+            bankAccount.withdraw(25);
+            bankAccount.withdraw(45);
+        } );
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+        t1.join();
+        t2.join();
+        t3.join();
+        System.out.println(bankAccount.getTotal());
     }
+
+
 }
--- src/main/java/SavingsAccount.java
+++ src/main/java/SavingsAccount.java
@@ -2,12 +2,26 @@ public class SavingsAccount {
 
   private long total = 0;
 
-  public boolean withdraw(long amount) {
-      
+  public synchronized boolean withdraw(long amount) {
+      long temp_total = getTotal() - amount;
+      boolean bool = false;
+
+      if(temp_total >= 0){
+        total = temp_total;
+        System.out.println("Current Balance: $" + getTotal());
+        bool = true;
+      } else if (temp_total < 0) {
+        System.out.println("Insufficient Funds");
+      }else{
+        System.out.print("Error. Please try again.");
+      }
+
+      return bool;
   }
 
-  public void deposit(long amount) {
-
+  public synchronized void deposit(long amount) {
+      total += amount;
+      System.out.println("Current Balance: $" + getTotal());
   }
 
   public long getTotal() {
